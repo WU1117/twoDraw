741fcdd0e29909afd0d38e980ead35f265d48840
WU1117 <657753588@qq.com>
2018-05-15 10:16:52 +0800

    Update loadBall.js

--- js/loadBall.js
+++ js/loadBall.js
@@ -1,29 +1,35 @@
-		function loadBallObjFile(url)
+		var index=0;
+function loadBallObjFile(url)
 		{
 		    var req = new XMLHttpRequest();
-		    req.onreadystatechange = function () { processBallLoadObj(req) };
-		    req.open("GET", url, true);
+		    req.onreadystatechange = function () { processBallLoadObj(req,url.split("|")[1]) };
+		    req.open("GET", url.split("|")[0], true);
 		    req.responseType = "text";
 		    req.send(null);
 		}
-		function processBallLoadObj(req)
+		function processBallLoadObj(req,name)
 		{
 		    if (req.readyState == 4) 
 		    {
-		        var objStr = req.responseText;	       
+		        var objStr = req.responseText;
 		        var dataTemp=fromObjStrToObjectData(objStr);	
-		        objArr.push(dataTemp);
+		        objArr[name]=dataTemp;
+                index++;
 		    }
 		}
-                function createObject(){
-			if(shaderFlag&&objArr.length==2){
-                ooTriBall=new ObjObjectBall(gl,objArr[0].vertices,objArr[0].normals,objArr[0].texcoords);
-                plane=new ObjObjectPlane(gl,objArr[1].vertices,objArr[1].texcoords);
+
+        function createObject(){
+			if(shaderFlag&&index==2){
+                ooTriBall=new ObjObjectBall(gl,objArr["ball"].vertices,objArr["ball"].normals,objArr["ball"].texcoords);
+                plane=new ObjObjectPlane(gl,objArr["plane"].vertices,objArr["plane"].texcoords);
                 celestial=new Celestial(gl,500,shaderProgArray[1]);
                 celestial1=new Celestial(gl,200,shaderProgArray[1]);
 			}else{
                 setTimeout(function(){createObject();},10);
 			}
 		}
+
+
+
 
 
